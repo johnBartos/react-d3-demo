bc7d4598c85cd502f40ed1ada4acfb1283f15071
John Bartos <jbartos7@gmail.com>
2016-02-01 22:11:00 -0500

    Basic d3 animation

--- src/Visualization.jsx
+++ src/Visualization.jsx
@@ -1,8 +1,41 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDom from 'react-dom';
+import d3 from 'd3';
 import dimensionable from './dimensionable';
 
+const draw = (container, containerHeight, containerWidth) => {
+  container.selectAll('*').remove();
+
+  const margin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
+  };
+
+  const height = containerHeight;
+  const width = containerWidth;
+
+  const svg = container.append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+  const circle = svg.append('circle')
+    .attr('cx', width / 2)
+    .attr('cy', height / 2)
+    .attr('r', 50);
+
+  circle.transition()
+  .duration(1000)
+  .delay(500)
+  .attr('cy', height - 50)
+  .transition()
+  .attr('cx', width - 50);
+};
+
 const style = {
-  height: '200px'
+  height: 200,
+  border: '1px solid red'
 };
 
 class Visualization extends Component {
@@ -15,11 +48,16 @@ class Visualization extends Component {
     width: PropTypes.number.isRequired
   };
 
+  componentDidMount() {
+    this.setState({ container: d3.select(ReactDom.findDOMNode(this)) });
+  }
+
   render() {
-    console.log(this.props.width);
-    return (
-      <div style={style}>yoyoyo</div>
-    );
+    if (this.state) {
+      draw(this.state.container, this.props.height, this.props.width);
+    }
+
+    return <div className="visualization-container jiggle" style={style}></div>;
   }
 }
 
